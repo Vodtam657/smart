8c8772a0a1026f1f0d2433f72086c705edea273f
sauceTartarys <vladislavserafim81@gmail.com>
2024-01-20 18:11:50 +0200

    ww

--- venv.py
+++ venv.py
@@ -5,7 +5,7 @@ window = QWidget()
 window.resize(900,700)
 
 zambtn = QPushButton("Створити замітку")
-delitbtn = QPushButton("Видалити замітку")
+delete = QPushButton("Видалити замітку")
 savebtp = QPushButton("Зберегти замітку")
 vikrbtn = QPushButton("Відкріпити від замітки")
 dodatubtn = QPushButton("Додати до замітки")
@@ -26,6 +26,22 @@ kkk = QLabel("Список тегів")
 mainline = QHBoxLayout()
 k1line = QVBoxLayout()
 k2line = QVBoxLayout()
+mainline.addLayout(k1line)
+mainline.addLayout(k2line)
 
+k1line.addWidget(text)
+k2line.addWidget(dodatubtn)
+k2line.addWidget(list1)
+k2line.addWidget(list2)
+k2line.addWidget(zambtn)
+k2line.addWidget(delete)
+k2line.addWidget(savebtp)
+
+mainline.addWidget(lll)
+
+
+
+
+window.setLayout(mainline)
 window.show()
 app.exec()
